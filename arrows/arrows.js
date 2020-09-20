8fbcd2cd21a94eb37a152e1477e5b0a914cfaf6f
Mathieu PATUREL <australie.p@gmail.com>
2020-09-20 13:50:22 +1000

    better API to create arrows

--- arrows/arrows.js
+++ arrows/arrows.js
@@ -1,4 +1,4 @@
-const arrows = (function () {
+const Arrow = (function () {
 
 const XMLNS = "http://www.w3.org/2000/svg"
 const POINT_SIZE = 16
@@ -13,32 +13,35 @@ const assert = (condition) => {
     }
 }
 
-const getRect = elem => {
-    const rects = elem.getClientRects()
-    assert(rects.length === 1)
-    return rects[0]
-}
-
-const getConnectPoints = rect => {
-    return [
-        {x: rect.left + rect.width / 2, y: rect.top},
-        {x: rect.left + rect.width / 2, y: rect.bottom},
-        {x: rect.left, y: rect.top + rect.height / 2},
-        {x: rect.right, y: rect.top + rect.height / 2}
-    ]
-}
-
 const squareDistance = (a, b) => {
     return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)
 }
 
-return {
-    connect: (a, b) => {
-        const recta = getRect(a)
-        const rectb = getRect(b)
+class Arrow {
+    constructor() {
+        this.from = document.createElement('div')
+        this.from.classList.add('point', 'from')
+        document.body.appendChild(this.from)
 
-        const connectPointsA = getConnectPoints(recta)
-        const connectPointsB = getConnectPoints(rectb)
+        this.to = document.createElement('div')
+        this.to.classList.add('point', 'to')
+        document.body.appendChild(this.to)
+
+        this.svg = document.createElementNS(XMLNS, "svg")
+        this.svg.classList.add('svg-arrow')
+
+        this.path = document.createElementNS(XMLNS, "path")
+        this.svg.appendChild(this.path)
+        document.body.appendChild(this.svg)
+
+    }
+
+    connect(a, b) {
+        const recta = this._getRect(a)
+        const rectb = this._getRect(b)
+
+        const connectPointsA = this._getConnectPoints(recta)
+        const connectPointsB = this._getConnectPoints(rectb)
 
         // only 16 possibilities, that's quick
         let mindistancepoints = [connectPointsA[0], connectPointsB[0]]
@@ -57,45 +60,39 @@ return {
         const pointFrom = mindistancepoints[0]
         const pointTo = mindistancepoints[1]
 
-        const from = document.createElement('div')
-        const to = document.createElement('div')
-        from.classList.add('point', 'from')
-        to.classList.add('point', 'to')
+        // const from = document.createElement('div')
+        // const to = document.createElement('div')
+        // from.classList.add('point', 'from')
+        // to.classList.add('point', 'to')
 
-        from.style.left = pointFrom.x - (POINT_SIZE / 2) + 'px'
-        from.style.top = pointFrom.y - (POINT_SIZE / 2) + 'px'
+        this.from.style.left = pointFrom.x - (POINT_SIZE / 2) + 'px'
+        this.from.style.top = pointFrom.y - (POINT_SIZE / 2) + 'px'
 
-        to.style.left = pointTo.x - (POINT_SIZE / 2) + 'px'
-        to.style.top = pointTo.y - (POINT_SIZE / 2) + 'px'
-        document.body.appendChild(from)
-        document.body.appendChild(to)
-
+        this.to.style.left = pointTo.x - (POINT_SIZE / 2) + 'px'
+        this.to.style.top = pointTo.y - (POINT_SIZE / 2) + 'px'
 
         const horizontalDistance = Math.abs(pointFrom.x - pointTo.x)
         const verticalDistance = Math.abs(pointFrom.y - pointTo.y)
 
         // create a new svg element
-        const svg = document.createElementNS(XMLNS, "svg")
-        svg.setAttributeNS(null, "viewbox", `0 0 ${horizontalDistance} ${verticalDistance}`)
-        svg.setAttributeNS(null, "width", horizontalDistance)
-        svg.setAttributeNS(null, "height", verticalDistance)
+        this.svg.setAttributeNS(null, "viewbox", `0 0 ${horizontalDistance} ${verticalDistance}`)
+        this.svg.setAttributeNS(null, "width", horizontalDistance)
+        this.svg.setAttributeNS(null, "height", verticalDistance)
 
         const topleft = {
             x: Math.min(pointFrom.x, pointTo.x),
             y: Math.min(pointFrom.y, pointTo.y)
         }
 
-        svg.classList.add('svg-arrow')
-        svg.style.left = topleft.x + 'px'
-        svg.style.top = topleft.y + 'px'
+        this.svg.style.left = topleft.x + 'px'
+        this.svg.style.top = topleft.y + 'px'
 
         // set it from JavaScript because otherwise nothing works, and I've got
         // a feeling this would be a source of headache
-        svg.style.overflow = "visible"
+        this.svg.style.overflow = "visible"
 
-        const path = document.createElementNS(XMLNS, "path")
-        path.setAttributeNS(null, "fill", "none")
-        path.setAttributeNS(null, "stroke", "red")
+        this.path.setAttributeNS(null, "fill", "none")
+        this.path.setAttributeNS(null, "stroke", "red")
 
         const svgFrom = {
             x: pointFrom.x - topleft.x,
@@ -109,7 +106,7 @@ return {
             (pointFrom.x === recta.left || pointFrom.x === recta.right) &&
             (pointTo.x === rectb.left || pointTo.x === rectb.right)
         ) {
-            path.setAttributeNS(null, "d", `
+            this.path.setAttributeNS(null, "d", `
                 M ${svgFrom.x},${svgFrom.y}
                 l ${(svgTo.x - svgFrom.x) / 2},0
                 l 0,${(svgTo.y - svgFrom.y)}
@@ -119,20 +116,20 @@ return {
             (pointFrom.y === recta.top || pointFrom.y === recta.bottom) &&
             (pointTo.y === rectb.top || pointTo.y === rectb.bottom)
         ) {
-            path.setAttributeNS(null, "d", `
+            this.path.setAttributeNS(null, "d", `
                 M ${svgFrom.x},${svgFrom.y}
                 l 0,${(svgTo.y - svgFrom.y) / 2}
                 l ${svgTo.x - svgFrom.x},0
                 L ${svgTo.x},${svgTo.y}
             `)
         } else if (pointFrom.y === recta.top || pointFrom.y === recta.bottom) {
-            path.setAttributeNS(null, "d", `
+            this.path.setAttributeNS(null, "d", `
                 M ${svgFrom.x},${svgFrom.y}
                 l 0,${svgTo.y - svgFrom.y}
                 L ${svgTo.x},${svgTo.y}
             `)
         } else if (pointFrom.x === recta.left || pointFrom.x === recta.right) {
-            path.setAttributeNS(null, "d", `
+            this.path.setAttributeNS(null, "d", `
                 M ${svgFrom.x},${svgFrom.y}
                 l ${svgTo.x - svgFrom.x},0
                 L ${svgTo.x},${svgTo.y}
@@ -141,14 +138,23 @@ return {
             console.error(pointFrom, pointTo)
             throw new Error("unexpected case")
         }
+    }
 
-        svg.appendChild(path)
-        document.body.appendChild(svg)
-    },
+    _getRect(elem) {
+        const rects = elem.getClientRects()
+        assert(rects.length === 1)
+        return rects[0]
+    }
 
-    update: (arrow, a, b) => {
-        throw new Error("not implemented")
+    _getConnectPoints(rect) {
+        return [
+            {x: rect.left + rect.width / 2, y: rect.top},
+            {x: rect.left + rect.width / 2, y: rect.bottom},
+            {x: rect.left, y: rect.top + rect.height / 2},
+            {x: rect.right, y: rect.top + rect.height / 2}
+        ]
     }
 }
+return Arrow
 
 })()
