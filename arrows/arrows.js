ea36eaacf4e08c43a42aacaf7620c7e3d546f75e
Mathieu PATUREL <australie.p@gmail.com>
2020-09-20 12:14:57 +1000

    choose connects points based on min euclid distance

--- arrows/arrows.js
+++ arrows/arrows.js
@@ -19,8 +19,59 @@ const getRect = elem => {
     return rects[0]
 }
 
+const getConnectPoints = rect => {
+    return [
+        {x: rect.left + rect.width / 2, y: rect.top},
+        {x: rect.left + rect.width / 2, y: rect.bottom},
+        {x: rect.left, y: rect.top + rect.height / 2},
+        {x: rect.right, y: rect.top + rect.height / 2}
+    ]
+}
+
+const squareDistance = (a, b) => {
+    return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)
+}
+
 return {
     connect: (a, b) => {
+        const recta = getRect(a)
+        const rectb = getRect(b)
+
+        const connectPointsA = getConnectPoints(recta)
+        const connectPointsB = getConnectPoints(rectb)
+
+        // only 16 possibilities, that's quick
+        let mindistancepoints = [connectPointsA[0], connectPointsB[0]]
+        for (let i = 0; i < connectPointsA.length; i++) {
+            for (let j = 0; j < connectPointsB.length; j++) {
+                const distance = squareDistance(connectPointsA[i], connectPointsB[j])
+                const mindistance = squareDistance(mindistancepoints[0], mindistancepoints[1])
+
+                if (distance < mindistance) {
+                    mindistancepoints[0] = connectPointsA[i]
+                    mindistancepoints[1] = connectPointsB[j]
+                }
+            }
+        }
+
+        const pointFrom = mindistancepoints[0]
+        const pointTo = mindistancepoints[1]
+
+        const from = document.createElement('div')
+        const to = document.createElement('div')
+        from.classList.add('point', 'from')
+        to.classList.add('point', 'to')
+
+        from.style.left = pointFrom.x - (POINT_SIZE / 2) + 'px'
+        from.style.top = pointFrom.y - (POINT_SIZE / 2) + 'px'
+
+        to.style.left = pointTo.x - (POINT_SIZE / 2) + 'px'
+        to.style.top = pointTo.y - (POINT_SIZE / 2) + 'px'
+        document.body.appendChild(from)
+        document.body.appendChild(to)
+    },
+
+    connectOld: (a, b) => {
         const recta = getRect(a)
         const rectb = getRect(b)
 
@@ -82,6 +133,7 @@ return {
         svg.classList.add('svg-arrow')
         svg.style.left = pointFrom.x + 'px'
         svg.style.top = pointFrom.y + 'px'
+
         // set it from JavaScript because otherwise nothing works, and I've got
         // a feeling this would be a source of headache
         svg.style.overflow = "visible"
@@ -108,8 +160,8 @@ return {
         svg.appendChild(path)
 
         document.body.appendChild(svg)
-
     },
+
     update: (arrow, a, b) => {
         throw new Error("not implemented")
     }
